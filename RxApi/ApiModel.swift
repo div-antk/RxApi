cc20b44634060df064bf9ae3bdd3f717be067528
div-antk <51283468+owaii@users.noreply.github.com>
2021-05-18 23:42:19 +0900

    一度更地にする

--- RxApi/ApiModel.swift
+++ RxApi/ApiModel.swift
@@ -8,43 +8,4 @@
 import Foundation
 import RxSwift
 
-struct SearchResponce: Decodable {
-    let query: Query
-    
-    struct Query: Decodable {
-        let search: [ApiModel]
-    }
-}
 
-struct ApiModel {
-    let id: String
-    let title: String
-    let url: URL
-}
-
-extension ApiModel: Decodable {
-    
-    private enum CodingKeys: String, CodingKey {
-        
-        // WikipediaAPIに準拠
-        case id = "padeid"
-        case title
-    }
-    
-    init(from decoder: Decoder) throws {
-        
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.id = String(try container.decode(Int.self, forKey: .id))
-        self.title = try container.decode(String.self, forKey: .title)
-        self.url = URL(string: "https://ja.wikipedia.org/w/input.php?curid=\(id)")!
-    }
-}
-
-// protocol Equatableに準拠して比較できるようにする
-extension ApiModel: Equatable {
-    
-    static func == (lhs: ApiModel, rhs: ApiModel) -> Bool {
-        return lhs.id == rhs.id
-    }
-}
--- RxApi/ViewController.swift
+++ RxApi/ViewController.swift
@@ -15,49 +15,13 @@ class ViewController: UIViewController, StoryboardInstantiatable {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var textField: UITextField!
-    
-    // VM初期化時、動作に依存するAPI実行のオブジェクトを注入
-    // テスト実行時にモック用のオブジェクトと置き換えて、ニセの通信によってテスト時間を短縮するため
-    private let viewModel = SearchViewModel(
-        wikipediaAPI: DefaultAPI(URLSession: .shared)
-    )
         
     private let disposeBag = DisposeBag()
     
-    private let cellReuseId = "sampleCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let input = SearchViewModel.Input(
-            searchWord: textField.rx.text.orEmpty.asObservable()
-        )
-        
-        let output = viewModel.transform(input: input)
-        
-        output.searchDescription.bind(to: navigationItem.rx.title)
-            .disposed(by: disposeBag)
-        
-        output.wikipage.bind(to: tableView.rx.items(cellIdentifier: TableViewCell.reusableIdentifier)) {
-            index, result, cell in
-            cell.textLabel?.text = result.title
-        }.disposed(by: disposeBag)
-        
-//        let cell = UINib(nibName: TableViewCell.reusableIdentifier, bundle: nil)
-//        tableView.register(UINib(nibName: TableViewCell.reusableIdentifier, bundle: nil), forCellReuseIdentifier.)
-        
-        self.tableView.register(UINib(nibName: TableViewCell.reusableIdentifier, bundle: nil), forCellReuseIdentifier: TableViewCell.reusableIdentifier)
-
-        // テキストフィールドに入力された文字をvmにbind
-//        textField.rx.text.orEmpty
-//            .filter{$0.count >= 1}
-//            .debounce(.microseconds(5), scheduler: MainScheduler.instance)
-//            .asDriver(onErrorDriveWith: Driver.empty())
-//            .drive(viewModel.self)
-//            .disposed(by: disposeBag)
-        
-        // イベントの検索結果のストリームを購読する
-        
         
     }
 
