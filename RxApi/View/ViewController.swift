72164569789df13868c572a130caa8cd04c925ff
div-antk <51283468+owaii@users.noreply.github.com>
2021-05-20 23:52:23 +0900

    VC編集中

--- RxApi/View/ViewController.swift
+++ RxApi/View/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import Moya
 import Instantiate
 import InstantiateStandard
 
@@ -16,12 +17,32 @@ class ViewController: UIViewController, StoryboardInstantiatable {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchTextField: UITextField!
     
+    let provider = MoyaProvider<Mtg>()
+    
+    state = .error
+    state = .loading
+    
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+  
         
+        provider.request(.cards) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let response):
+                do {
+                    print(try response.mapJSON())
+                } catch {
+                    self.state = .error
+                }
+            case .failure:
+                self.state = .error
+            }
+        }
     }
 
 
