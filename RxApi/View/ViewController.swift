791a8b40979953c185c84e4429e78bbc6d90083c
div-antk <51283468+owaii@users.noreply.github.com>
2021-06-05 23:35:38 +0900

    UIの修正

--- RxApi/View/ViewController.swift
+++ RxApi/View/ViewController.swift
@@ -15,8 +15,9 @@ import Kingfisher
 
 class ViewController: UIViewController, StoryboardInstantiatable {
     
-    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var reloadButton: UIButton!
     
     var cards: CardListResponse?
     
@@ -28,14 +29,13 @@ class ViewController: UIViewController, StoryboardInstantiatable {
         let provider = MoyaProvider<MtgAPI>()
         
         provider.request(.cards) { (result) in
-            
             switch result {
             case .success(let response):
                 let data = response.data
                 do {
                     let cards = try JSONDecoder().decode(CardListResponse.self, from: data)
                     self.cards = cards
-                    self.collectionView.reloadData()
+                    self.cardImage()
                 } catch(let error) {
                     print(error)
                 }
@@ -43,50 +43,12 @@ class ViewController: UIViewController, StoryboardInstantiatable {
                 print(error)
             }
         }
-        
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        collectionView.register(UINib(nibName: CardCollectionViewCell.reusableIdentifier, bundle: nil), forCellWithReuseIdentifier: CardCollectionViewCell.reusableIdentifier)
-    }
-}
-
-extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        return (cards?.cards.count)!
-//        print(cards?.cards)
-        return 1
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        if let count = cards?.cards.count {
-//            return count
-//        }
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCollectionViewCell.reusableIdentifier, for: indexPath) as! CardCollectionViewCell
-        
+    func cardImage() {
         if let cardImage = cards?.cards.randomElement()?.imageUrl {
-            cell.cardImageView.kf.setImage(with: URL(string: cardImage))
+            print(cardImage)
+            cardImageView.kf.setImage(with: URL(string: cardImage))
         }
-        return cell
     }
-}
-
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    // カスタムセルのサイズ
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
-        return CGSize(width: view.frame.width - 40, height: view.frame.height - 60)
-    }
-
-//    // 各カスタムセル外枠の余白
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//
-//        return UIEdgeInsets(top: 16, left: 16, bottom: 0, right: 16)
-//    }
 }
